572c0d783a807b0dc5368ac12d948c49cecf7cfa
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2023-11-27 19:40:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+let tanya = $('#tanya');
+let john = $('#john');
 
+$('button').click(function (e) { 
+  e.preventDefault();
+  $(tanya).toggleClass('display-none');
+  $(john).toggleClass('display-none');
+});
